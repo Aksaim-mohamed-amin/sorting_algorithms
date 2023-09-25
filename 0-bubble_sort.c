08fcc3e4cbe832e12dd27c4e948120b5808dd0b2
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-09-25 04:51:50 -0700

    task 0 (Bubble Sort)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,6 +19,7 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = false;
+
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -26,8 +27,9 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
-				print_array(array, size);
+
 				swapped = true;
+				print_array(array, size);
 			}
 		}
 		if (!swapped)
